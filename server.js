0040cc92b27389cbf9f513e87e27e6b751622cba
James Langford <derivative7117@live.com>
2018-01-02 10:06:45 -0700

    updated package.json

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(bodyParser.json())
 require('./routes/getData')(app)
 require('./routes/updateData')(app)
 //<-----production ----->
-app.use(express.static(path.join(__dirname, 'client/build')))
-app.get('*', (req, res) => {
-  res.sendFile((path.join(__dirname, 'client/build/index.html')))
-})
+// app.use(express.static(path.join(__dirname, 'client/build')))
+// app.get('*', (req, res) => {
+//   res.sendFile((path.join(__dirname, 'client/build/index.html')))
+// })
 //<--------------------->
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => { console.log('Connected To Mongo Database')})
